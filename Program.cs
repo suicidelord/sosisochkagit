97ddbbed3591b63dfc8dfa93a7cb7fbb1d1f9eb3
Suicidelord <isip_n.d.gorlulenko@mpt.ru>
2024-04-04 22:53:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using NLog;
+using NLog.Web;
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+logger.Info("startlogger");
+
+try
+{
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Logging.AddConsole();
+    builder.Logging.AddDebug();
+    builder.Logging.AddEventSourceLogger();
+
+    builder.Host.UseNLog();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    logger.Error(ex);
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
